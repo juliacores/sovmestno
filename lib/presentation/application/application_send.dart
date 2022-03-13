d31bd980e8d46b635c4c63052d1e0fa9570a8b49
Roman <225000@niuitmo.ru>
2022-03-13 17:43:32 +0300

    things

--- lib/presentation/application/application_send.dart
+++ lib/presentation/application/application_send.dart
@@ -14,47 +14,45 @@ class ApplicationSend extends StatefulWidget {
 class _ApplicationSendState extends State<ApplicationSend> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 70,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 70,
+        ),
+        const Text(
+          'Это сообщение будет отправлено ментору:',
+          style: Styles.colorBlack14bold,
+        ),
+        Container(
+          child: const Padding(
+            padding: EdgeInsets.all(14.0),
+            child: Text('какой то тест', style: Styles.hintColor14Bold),
           ),
-          const Text(
-            'Это сообщение будет отправлено ментору:',
-            style: Styles.colorBlack14bold,
+          height: 333,
+          width: 476,
+          decoration: BoxDecoration(
+            // color: Colors.white,
+            border: Border.all(color: AppColors.hintColor),
+            borderRadius: BorderRadius.circular(8.0),
           ),
-          Container(
-            child: const Padding(
-              padding: EdgeInsets.all(14.0),
-              child: Text(ConstText.cardText, style: Styles.hintColor14Bold),
+        ),
+        const SizedBox(
+          height: 33,
+        ),
+        Row(
+          children: const [
+            SizedBox(
+              width: 234,
             ),
-            height: 333,
-            width: 476,
-            decoration: BoxDecoration(
-              // color: Colors.white,
-              border: Border.all(color: AppColors.hintColor),
-              borderRadius: BorderRadius.circular(8.0),
+            ApplicationButtonWidget(
+              name: 'Отправить',
+              width: 226,
+              height: 40,
             ),
-          ),
-          const SizedBox(
-            height: 33,
-          ),
-          Row(
-            children: const [
-              SizedBox(
-                width: 234,
-              ),
-              ApplicationButtonWidget(
-                name: 'Отправить',
-                width: 226,
-                height: 40,
-              ),
-            ],
-          ),
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
